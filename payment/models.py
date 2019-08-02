36d25a3111b97dcc069bfe8bdcc0ae88458e8b76
Gregory Vasiliev <lamgren@gmail.com>
2019-08-02 14:42:31 +0300

    added django_robokassa

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Payment(models.Model):
+    TINKOFF = 'tin'
+    ROBOKASSA = 'rob'
+    MERCH = [
+        (TINKOFF, 'Tinkoff'),
+        (ROBOKASSA, 'Robokassa'),
+    ]
+    merch = models.CharField(max_length=3, choices=MERCH, default=ROBOKASSA)
+    app = models.ForeignKey('AppOrder', on_delete=models.DO_NOTHING,
+                            related_name='payment',
+                            related_query_name='payment')
+    amount = models.DecimalField(max_digits=8, decimal_placed=2)
+    currency = models.CharField(max_length=5, default='RUB')
+    
