31448b39e634cd8b989d3bd6ae23936dedc4dc7d
Катюня <user@iMac-user.lan>
2019-09-22 18:32:58 +0300

    fixed urls

--- frontend/urls.py
+++ frontend/urls.py
@@ -9,8 +9,9 @@ urls = [
     url(r'^$',TemplateView.as_view(template_name='index.html')),
     url(r'^chat/',TemplateView.as_view(template_name='index.html')),
     url(r'^blog/',TemplateView.as_view(template_name='index.html')),
-    url(r'^matrix/',TemplateView.as_view(template_name='index.html')),    
+    url(r'^matrix/',TemplateView.as_view(template_name='index.html')),
     url(r'^services/',TemplateView.as_view(template_name='index.html')),
     url(r'^profile/',TemplateView.as_view(template_name='index.html')),
     url(r'^admin/school/*',TemplateView.as_view(template_name='index.html')),
+    url(r'^options/',TemplateView.as_view(template_name='index.html')),
 ]
