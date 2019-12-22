7890dc7bf93bfe9460a3de775865cf30530809d4
Gregory Vasiliev <lamgren@gmail.com>
2019-12-22 22:55:41 +0700

    changed flow field data in serializer for PromoCodes

--- promocode/serializers.py
+++ promocode/serializers.py
@@ -3,6 +3,12 @@ from .models import PromoCode
 
 class PromoCodesSerializer(serializers.ModelSerializer):
 
+    flow = serializers.SerializerMethodField()
+
+
+    def get_flow(self, obj):
+        return '{} {}'.format(obj.flow.flow, obj.flow.flow_name)
+
     class Meta:
         model = PromoCode
         fields = '__all__'
