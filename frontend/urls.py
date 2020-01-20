50b53b5e9b75d3637a016906a3b20f1060f17fde
Gregory <greg@134-0-116-46.ovz.vps.regruhosting.ru>
2020-01-20 21:51:32 +0000

    premerge

--- frontend/urls.py
+++ frontend/urls.py
@@ -6,8 +6,8 @@ from django.views.generic import TemplateView, RedirectView
 
 urls = [
 #    url(r'^$',serve,{'path':'index.html'}),
-#    url(r'^$',TemplateView.as_view(template_name='index.html')),
-    url(r'^$',RedirectView.as_view(url="/school/apply")),
+    url(r'^$',TemplateView.as_view(template_name='index.html')),
+#    url(r'^$',RedirectView.as_view(url="/school/apply")),
 #RedirectView.as_view(url=reverse_lazy('my_named_pattern')
     url(r'^chat/',TemplateView.as_view(template_name='index.html')),
     url(r'^blog/',TemplateView.as_view(template_name='index.html')),
