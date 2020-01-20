cce817166d08c5191c71e75193f49a523c938ecd
Gregory <greg@134-0-116-46.ovz.vps.regruhosting.ru>
2020-01-20 15:33:18 +0000

    added events models to admin.site django

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(OfflineEvent)
+admin.site.register(EventTicketTemplate)
+admin.site.register(Ticket)
 
 # Register your models here.
