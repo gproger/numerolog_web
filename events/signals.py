1da1a2c2c63f29280038bcf89e87880de026f366
Kate Barteneva <kate_barteneva@iMacKatarteneva.lan>
2020-01-21 02:41:16 +0300

    define callback before connect

--- events/signals.py
+++ events/signals.py
@@ -1,9 +1,10 @@
 from django_tinkoff_merchant.signals import payment_update
 from .models import Ticket
 
-payment_update.connect(payment_callback)
-
 def payment_callback(sender, **kwargs):
     payment = kwargs.get('payment',None)
     print("Request finished!")
     print(payment)
+
+
+payment_update.connect(payment_callback)
