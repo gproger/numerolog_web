18944144c054b3bf90c6a87b2a27fd69371c4df7
Kate Barteneva <kate_barteneva@iMacKatarteneva.lan>
2020-01-21 06:27:56 +0300

    another fix

--- promocode/serializers.py
+++ promocode/serializers.py
@@ -11,7 +11,7 @@ class PromoCodesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = PromoCode
-        exclude = 'evticket'
+        exclude = ['evticket']
 
 
 class PromoCodesTicketSerializer(serializers.ModelSerializer):
