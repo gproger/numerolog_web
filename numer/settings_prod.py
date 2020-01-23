ef45ea53dce9ba9f3e0d004246da0385d8649307
Gregory <greg@134-0-116-46.ovz.vps.regruhosting.ru>
2020-01-23 18:21:26 +0000

    prod changes and prod settings; and prod start wsgi file

--- numer/settings_prod.py
+++ numer/settings_prod.py
@@ -16,6 +16,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+#DEBUG=True
 
 # Define placeholder gettext function
 # This function will mark strings in settings visible to makemessages
