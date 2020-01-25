8d1484777d0de543b7364b1c54c8d16793813895
Kate Barteneva <kate_barteneva@iMacKatarteneva.lan>
2020-01-25 17:05:48 +0300

    changed curator admin

--- schoolform/admin.py
+++ schoolform/admin.py
@@ -3,7 +3,15 @@ from .models import SchoolAppForm, SchoolAppFlow, SchoolAppCurator
 
 admin.site.register(SchoolAppForm)
 admin.site.register(SchoolAppFlow)
-admin.site.register(SchoolAppCurator)
+
+@admin.register(SchoolAppCurator)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ['id', 'email','phone','first_name',
+        'middle_name', 'last_name','instagramm','bid','flow__flow_name','curator','expert']
+    search_fields = ['phone','email','first_name','last_name','middle_name']
+    list_filter = ['curator','expert','flow__flow_name']
+
+
 
 
 # Register your models here.
