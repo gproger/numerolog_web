4c4c25458228d850ae3b5a950bcc26e8a585571d
Kate Barteneva <kate_barteneva@iMacKatarteneva.lan>
2020-01-25 13:22:02 +0300

    test

--- events/tasks.py
+++ events/tasks.py
@@ -1,9 +1,10 @@
-from events.models import Ticket
+
 from emails.emails import mail_user
 from django.template.loader import render_to_string
 from django.template import Template
 from django.template import Context
 from weasyprint import HTML
+from events.models import Ticket
 
 from myproject.celery import app
 from django.conf import settings
