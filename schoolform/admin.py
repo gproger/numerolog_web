c3d7e44c5323d5a484def9ee5701ad08dc3ab3c1
Kate Barteneva <kate_barteneva@iMacKatarteneva.lan>
2020-01-25 17:08:57 +0300

    change filter flow_name showing

--- schoolform/admin.py
+++ schoolform/admin.py
@@ -4,10 +4,13 @@ from .models import SchoolAppForm, SchoolAppFlow, SchoolAppCurator
 admin.site.register(SchoolAppForm)
 admin.site.register(SchoolAppFlow)
 
+def flow_name(obj):
+    return obj.flow.flow_name
+
 @admin.register(SchoolAppCurator)
 class SchoolAppCuratorAdmin(admin.ModelAdmin):
     list_display = ['id', 'email','phone','first_name',
-        'middle_name', 'last_name','instagramm','bid','flow__flow_name','curator','expert']
+        'middle_name', 'last_name','instagramm','bid','flow_name','curator','expert']
     search_fields = ['phone','email','first_name','last_name','middle_name']
     list_filter = ['curator','expert','flow__flow_name']
 
