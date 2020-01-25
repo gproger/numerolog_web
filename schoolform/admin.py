d5e834e358585a78677847af8f51627918be4b79
Kate Barteneva <kate_barteneva@iMacKatarteneva.lan>
2020-01-25 18:02:17 +0300

    added counters for admin schoolappflow

--- schoolform/admin.py
+++ schoolform/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import F
 from .models import SchoolAppForm, SchoolAppFlow, SchoolAppCurator
 
 admin.site.register(SchoolAppForm)
@@ -17,12 +18,27 @@ class SchoolAppCuratorAdmin(admin.ModelAdmin):
 def registered(obj):
     return obj.schoolappform_set.count()
 
+def pre_pay(obj):
+    return obj.schoolappform_set.filter(payed_amount__gt=0).filter(payed_amount__lt=F('price')).count()
+
+def full_pay(obj):
+    return obj.schoolappform_set.filter(payed_amount=F('price')).count()
+
+def no_pay(obj):
+    return obj.schoolappform_set.filter(payed_amount=0).count()
+
 def curators(obj):
     return obj.schoolappcurator_set.count()
 
+registered.short_description = 'Всего'
+pre_pay.short_description = 'С предоплатой'
+full_pay.short_description = 'Оплачено'
+no_pay.short_description = 'Без оплаты'
+curators.short_description = 'Кураторов'
+
 @admin.register(SchoolAppFlow)
 class SchoolAppFlowAdmin(admin.ModelAdmin):
-    list_display = ['id','flow','flow_name','state','price',registered, curators]
+    list_display = ['id','flow','flow_name','state','price',registered, no_pay,pre_pay, full_pay,curators]
     list_filter = ['state']
 
 
