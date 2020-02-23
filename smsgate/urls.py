dc4c074b7900136a047836f8724f6919fefe4f96
Kate Barteneva <kate_barteneva@iMacKatarteneva.lan>
2020-02-23 13:56:57 +0300

    chaned name

--- smsgate/urls.py
+++ smsgate/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 
 from .views import SmsNotify, SMSVerifyPhone, SMSTestCode
 
-urlpatterns = [
+urls = [
     url(r'^smsnotify/', SmsNotify.as_view(), name='smsnotify'),
     url(r'^get_sms_code/', SMSVerifyPhone.as_view(),name='smsverifyphone'),
     url(r'^check_sms_code/', SMSTestCode.as_view(),name='smstestcode'),
