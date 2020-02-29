0bfaff7269fea29dcec514afd88901eb4d01074f
Gregory Vasiliev <lamgren@gmail.com>
2020-02-29 14:13:17 +0300

    added comment field for schoolform

--- schoolform/models.py
+++ schoolform/models.py
@@ -141,6 +141,7 @@ class SchoolAppForm(models.Model):
     pay_url_sended = models.NullBooleanField(default=False)
     payed_amount = models.PositiveIntegerField(default=0)
     phone_valid = models.NullBooleanField(default=False)
+    comment = models.TextField(required=False)
 
     def save(self, *args, **kwargs):
 
