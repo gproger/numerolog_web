f24758ab649fbaa5b53c8411358be998ee44995d
Gregory <greg@134-0-116-46.ovz.vps.regruhosting.ru>
2020-03-24 06:32:33 +0000

    fixes from production

--- schoolform/serializers.py
+++ schoolform/serializers.py
@@ -25,14 +25,17 @@ class SchoolAppFormCreateSerializer(serializers.ModelSerializer):
 class SchoolAppCuratorCreateSerializer(serializers.ModelSerializer):
     bid = serializers.DateField(format="%d.%m.%Y",input_formats=['%d.%m.%Y'])
     created = serializers.DateTimeField(format="%d.%m.%Y %H:%M:%S",input_formats=['%d.%m.%Y'], required=False)
+    flow = serializers.PrimaryKeyRelatedField(queryset=SchoolAppFlow.objects.all(),many=False)
+
 
     def validate_email(self, value):
         norm_value = value.lower()
         return norm_value
 
+
     class Meta:
         model = SchoolAppCurator
-        exclude = ['flow']
+        fields = '__all__'
 
 class SchoolAppFlowListSerializer(serializers.ModelSerializer):
     state = serializers.SerializerMethodField()
