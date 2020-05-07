bf123aee85b4d8ff64580a2f20b9cad72a544fca
Kate Barteneva <kate_barteneva@iMacKatarteneva.lan>
2020-05-07 17:29:30 +0300

    remove required for form checkbox

--- schoolform/forms.py
+++ schoolform/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 class RefundForm(forms.Form):
     _selected_action = forms.CharField(widget=forms.MultipleHiddenInput)
-    is_percent = forms.BooleanField(initial=False, label=u'Возврат в процентах?')
+    is_percent = forms.BooleanField(initial=False, label=u'Возврат в процентах?', required=False)
     amount = forms.IntegerField(label=u'Сумма возврата', required=True, initial=0)
