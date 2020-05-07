a11db81a4662d6abfa199d3b32f8d7f9568a386b
Kate Barteneva <kate_barteneva@iMacKatarteneva.lan>
2020-05-07 17:15:25 +0300

    naming fix

--- schoolform/forms.py
+++ schoolform/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 class RefundForm(forms.Form):
     _selected_action = forms.CharField(widget=forms.MultipleHiddenInput)
-    is_percent = forms.BoolenField(default=false, label=u'Возврат в процентах?')
+    is_percent = forms.BooleanField(default=false, label=u'Возврат в процентах?')
     amount = forms.IntegerField(label=u'Сумма возврата')
