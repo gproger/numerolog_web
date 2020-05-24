272cf83a56af6cd49b40f44b61dd59a13719c584
Kate Barteneva <kate_barteneva@iMac-Kate-Barteneva.local>
2020-05-24 19:14:27 +0300

    tickets migration for userInfo

--- events/migrations/0010_create_userinfo.py
+++ events/migrations/0010_create_userinfo.py
@@ -5,6 +5,36 @@ from __future__ import unicode_literals
 from django.db import migrations
 
 
+def forwards(apps, schema_editor):
+    UserInfoModel = apps.get_model('users', 'UserInfo')
+    Ticket = apps.get_model('events','Ticket')
+
+    queryset = Ticket.objects.all()
+
+    for ticket in queryset:
+        u_info = UserInfoModel.objects.filter(phone=ticket._phone)
+        if u_info.first() is not None:
+            ticket.userinfo = u_info.first()
+            ticket.save()
+        else:
+            u_info = UserInfoModel.objects.filter(email=ticket._email)
+            if u_info.first() is not None:
+                ticket.userinfo = u_info.first()
+                ticket.save()
+            else:
+                u_info = UserInfoModel(
+                email=ticket._email,
+                phone=ticket._phone,
+                first_name=ticket._first_name,
+                middle_name=ticket._middle_name,
+                last_name=ticket._last_name,
+                )
+                u_info.save()
+                ticket.userinfo = u_info
+                ticket.save()
+        
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -12,4 +42,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(forwards, reverse_code=migrations.RunPython.noop),
     ]
