bf2f61bbef354dbd4ae63969f8371091c33812ec
Gregory <greg@134-0-116-46.ovz.vps.regruhosting.ru>
2020-12-30 21:46:41 +0000

    fixed urls for frontend; added facebook pixel code for main page

--- frontend/urls.py
+++ frontend/urls.py
@@ -6,8 +6,8 @@ from django.views.generic import TemplateView, RedirectView
 
 urls = [
 #    url(r'^$',serve,{'path':'index.html'}),
-#    url(r'^$',TemplateView.as_view(template_name='index.html')),
-    url(r'^$',RedirectView.as_view(url="/school/")),
+    url(r'^$',TemplateView.as_view(template_name='index.html')),
+#    url(r'^$',RedirectView.as_view(url="/school/")),
 #RedirectView.as_view(url=reverse_lazy('my_named_pattern')
     url(r'^chat/',TemplateView.as_view(template_name='index.html')),
     url(r'^blog/',TemplateView.as_view(template_name='index.html')),
@@ -16,7 +16,7 @@ urls = [
     url(r'^profile/',TemplateView.as_view(template_name='index.html')),
     url(r'^tos/',TemplateView.as_view(template_name='index.html')),
     url(r'^admin/school/*',TemplateView.as_view(template_name='index.html')),
-    url(r'^school/*',TemplateView.as_view(template_name='index.html')),
+    url(r'^schoolp/*',TemplateView.as_view(template_name='index.html')),
     url(r'^options/',TemplateView.as_view(template_name='index.html')),
     url(r'^forgotten-password/',TemplateView.as_view(template_name='index.html')),
     url(r'^pay/*',TemplateView.as_view(template_name='index.html')),
@@ -28,5 +28,6 @@ urls = [
     url(r'^sale/*',TemplateView.as_view(template_name='index.html')),
     url(r'^admin/experts/*',TemplateView.as_view(template_name='index.html')),
     url(r'^admin/events/*',TemplateView.as_view(template_name='index.html')),
+    url(r'^admin/services/*',TemplateView.as_view(template_name='index.html')),
 
 ]
